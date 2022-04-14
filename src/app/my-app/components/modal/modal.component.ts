e05d85dc5e680488417a04a43d81534a35511795
guschins <guschins@gmail.com>
2022-04-14 19:51:11 +0300

    del bug

--- src/app/my-app/components/modal/modal.component.ts
+++ src/app/my-app/components/modal/modal.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NgbModal, ModalDismissReasons, NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import {NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-modal',
@@ -14,7 +14,7 @@ export class ModalComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    throw new Error('Method not implemented.');
+    // throw new Error('Method not implemented.');
   }
 
 }
